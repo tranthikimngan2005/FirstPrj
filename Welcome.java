ecfd2513397d84b6c24d5cebfcbb088e6181eab9
Tran Thi Kim Ngan <23719511.ngan@student.iuh.edu.vn>
2024-08-14 19:33:11 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main (String[] ards)
+    {
+        System.out.println("hello tran thi kim ngan ");
+        System.out.println("welcome to the DHKTPM19A");
+
+    }
+}
